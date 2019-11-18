595748a53fd49417b37f04a8052936e60de8ef58
niteshsaini2418 <49919006+niteshsaini2418@users.noreply.github.com>
2019-11-18 23:58:12 +0530

    stack top

---
+++ stacktop.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Stack
+{     int arr[],top,capacity;
+    Stack(int size)
+    {
+    arr=new int[size];
+    top=-1;
+    capacity=size;
+    }
+    void push(int data)
+    { 
+        if(top>=capacity-1)
+        {
+            System.out.println("Stack is full");
+        }
+        else
+        {
+        top++;
+        arr[top]=data;
+        System.out.println(arr[top]+" Item pushed");
+        }
+    }
+
+
+}
+
+
+class stackop
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Stack s=new Stack(4);
+
+        s.push(5);
+        s.push(6);
+        s.push(7);
+        s.push(8);
+        s.push(9);
+      }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
