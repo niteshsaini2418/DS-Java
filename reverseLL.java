d6370447474a9b4a7d4d85499912fa69c0ed1487
niteshsaini2418 <49919006+niteshsaini2418@users.noreply.github.com>
2019-12-15 22:34:27 +0530

    reverse LL without recursion

---
+++ reverseLL.java
@@ -0,0 +1,58 @@
+class reverseLL { 
+  
+    static Node head; 
+  
+    static class Node { 
+  
+        int data; 
+        Node next; 
+  
+        Node(int d) 
+        { 
+            data = d; 
+            next = null; 
+        } 
+    } 
+  
+    Node reverse(Node node) 
+    { 
+        Node prev=null;
+        Node next=null;
+        Node current=node;
+
+        while(current!=null)
+        {
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        return prev;
+
+        
+    } 
+  
+    void printList(Node node) 
+    { 
+        while (node != null) { 
+            System.out.print(node.data + " "); 
+            node = node.next; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        reverseLL list = new reverseLL(); 
+        list.head = new Node(85); 
+        list.head.next = new Node(15); 
+        list.head.next.next = new Node(4); 
+        list.head.next.next.next = new Node(20); 
+  
+        System.out.println("Given Linked list"); 
+        list.printList(head); 
+        head = list.reverse(head); 
+        System.out.println(""); 
+        System.out.println("Reversed linked list "); 
+        list.printList(head); 
+    } 
+} 
